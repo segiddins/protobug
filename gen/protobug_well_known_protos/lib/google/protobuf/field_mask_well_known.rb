ef644dd8e3701188753b1b03f00c09726976a009
Samuel Giddins <segiddins@segiddins.me>
2024-03-25 13:08:43 +0000

    json field mask

--- gen/protobug_well_known_protos/lib/google/protobuf/field_mask_well_known.rb
+++ gen/protobug_well_known_protos/lib/google/protobuf/field_mask_well_known.rb
@@ -5,9 +5,29 @@ Google::Protobuf::FieldMask.class_eval do
     raise DecodeError, "expected string for #{full_name}, got #{json.inspect}" unless json.is_a? String
 
     paths = json.split(",").each do |field|
-      field.gsub!(/(?<!\A)([A-Z])/) { |m| "_#{m.downcase}" }
+      unless /\A[a-zA-Z]+\z/.match?(field)
+        raise Protobug::DecodeError,
+              "field mask has invalid characters from JSON: #{field.inspect}"
+      end
+
+      field.gsub!(/(?<!\A)([A-Z])/) do |m|
+        "_#{m.downcase}"
+      end
     end
     json = { "paths" => paths }
     super
+  end
+
+  def as_json(print_unknown_fields: false)
+    _ = print_unknown_fields
+
+    paths.map do |path|
+      unless /\A[a-z]+(_[a-z]+)*\z/.match?(path)
+        raise Protobug::EncodeError,
+              "cannot encode field mask paths to JSON with non-lowercase-alpha characters: #{path.inspect}"
+      end
+
+      path.gsub(/(?<!\A)(?:_([a-z]+))/, &:capitalize)
+    end.join(",")
   end
 end
